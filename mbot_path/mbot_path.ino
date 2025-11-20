95c57461ecfdddd39118b3b86bd705559ee26328
bd6003630 <bd6003630@ahschool.com>
2025-11-20 15:30:16 -0500

    Update mbot_path.ino

--- mbot_path/mbot_path.ino
+++ mbot_path/mbot_path.ino
@@ -5,12 +5,34 @@ MeDCMotor motor1(M1);
 MeDCMotor motor2(M2);
 MeRGBLed led(0,30);
 /*****************************/
-void move() {
+void moveFar() {
     led.setColor(0,255,0);
     led.show();
     motor1.run(100);
-    motor2.run(-100);
-    delay(5000);
+    motor2.run(-107);
+    delay(4750);
+    motor1.stop();
+    motor2.stop();
+    led.setColor(0,0,0);
+    led.show();
+}
+void moveClose() {
+    led.setColor(0,255,0);
+    led.show();
+    motor1.run(100);
+    motor2.run(-107);
+    delay(3200);
+    motor1.stop();
+    motor2.stop();
+    led.setColor(0,0,0);
+    led.show();
+}
+void moveMed() {
+    led.setColor(0,255,0);
+    led.show();
+    motor1.run(100);
+    motor2.run(-110);
+    delay(4000);
     motor1.stop();
     motor2.stop();
     led.setColor(0,0,0);
@@ -20,7 +42,7 @@ void turnLeft() {
     led.setColor(255,0,0);
     led.show();
     motor1.run(100);
-    delay(1100);
+    delay(1075);
     motor1.stop();
     led.setColor(0,0,0);
     led.show();
@@ -37,25 +59,42 @@ void turnRight() {
 void spin180() {
     led.setColor(255,0,0);
     led.show();
-    motor1.run(100);
-    delay(2000);
+    motor1.run(-100);
+    motor2.run(-107);
+    delay(1100);
     motor1.stop();
+    motor2.stop();
     led.setColor(0,0,0);
     led.show();
 }
-void moveBackward() {
+void backward() {
     led.setColor(0,255,0);
     led.show();
     motor1.run(-100);
     motor2.run(100);
-    delay(5000);
+    delay(1000);
+    motor1.stop();
+    motor2.stop();
+    led.setColor(0,0,0);
+    led.show();
+}
+void forward() {
+    led.setColor(0,255,0);
+    led.show();
+    motor1.run(100);
+    motor2.run(-100);
+    delay(1000);
     motor1.stop();
     motor2.stop();
     led.setColor(0,0,0);
     led.show();
 }
 void pause() {
-    delay(5000);
+    led.setColor(255,0,0);
+    led.show();
+    delay(1000);
+    led.setColor(0,0,0);
+    led.show();
 }
 /*****************************/
 void setup(){
@@ -66,9 +105,17 @@ void setup(){
 /*****************************/
 void loop() {
    delay(3000);
-   move();
+   moveFar();
+   pause();
    turnRight();
-   move();
+   pause();
+   moveClose();
+   pause();
    turnLeft();
-   move();
+   pause();
+   moveMed();
+   pause();
+   spin180();
+   forward();
+   backward();
 }

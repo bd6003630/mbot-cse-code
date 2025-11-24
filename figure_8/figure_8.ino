ebfdf5bbabcca9fcfc5602edd6dd591d9583f347
bd6003630 <bd6003630@ahschool.com>
2025-11-24 14:55:22 -0500

    Update figure_8.ino

--- figure_8/figure_8.ino
+++ figure_8/figure_8.ino
@@ -1,14 +1,22 @@
+#include <Arduino.h>
 #include <MeMCore.h>
+
 MeLineFollower lineFinder(PORT_2);
-
+MeDCMotor motor1(M1);
+MeDCMotor motor2(M2);
+MeRGBLed led(0,30);
+/*****************************/
 void setup() {
-  // put your setup code here, to run once:
-
+    led.setpin(13);
+    pinMode(A7,INPUT);
+    while(analogRead(A7) !=0);
 }
+/*****************************/
 void loop() {
   if (lineFinder.readSensor1()==0 && lineFinder.readSensor2()==0)
     {
       Serial.println("Sensor 1 on black and Sensor2 on black");
+      motor1.run
     }
   else if (lineFinder.readSensor1()==1 && lineFinder.readSensor2()==0)
     {

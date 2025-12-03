2166998f997109bf08df205165768cba71fba1f6
bd6003630 <bd6003630@ahschool.com>
2025-12-03 14:54:02 -0500

    Update traverse_maze.ino

--- traverse_maze/traverse_maze.ino
+++ traverse_maze/traverse_maze.ino
@@ -2,15 +2,21 @@
 #include <MeMCore.h>
 
 MeUltrasonicSensor ultraSonic(PORT_3);
+MeLineFollower lineFinder(PORT_2);
+MeDCMotor motor1(M1);
+MeDCMotor motor2(M2);
+MeRGBLed led(0,30);
 
 void setup() {
+  led.setpin(13);
+  pinMode(A7,INPUT);
+  while(analogRead(A7) !=0);
   Serial.begin(9600);
-
 }
 
 void loop() {
   Serial.print("Distance: ");
   Serial.print(ultraSonic.distanceCm());
   Serial.println(" cm");
-  delay(100);
+  delay(300);
 }

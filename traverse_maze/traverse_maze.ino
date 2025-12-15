dc5efaef3a63f9fd5e2ad65abd04bbe2d024cf8d
bd6003630 <bd6003630@ahschool.com>
2025-12-15 14:33:08 -0500

    Update traverse_maze.ino

--- traverse_maze/traverse_maze.ino
+++ traverse_maze/traverse_maze.ino
@@ -7,26 +7,45 @@ MeDCMotor motor1(M1);
 MeDCMotor motor2(M2);
 MeRGBLed led(0,30);
 /*****************************/
+void right() {
+  motor1.run(100);
+  motor2.run(100);
+  delay(550);
+  motor1.stop();
+  motor2.stop();
+}
+void fullTurn() {
+  motor1.run(100);
+  motor2.run(100);
+  delay(1000);
+  motor1.stop();
+  motor2.stop();
+}
 void move() {
   if (ultraSonic.distanceCm() > 10) {
     motor1.run(100);
     motor2.run(-100);
   }
 }
+/*****************************/
 void turn() {
   if (ultraSonic.distanceCm() < 10) {
-    motor1.run(100);
-    motor2.run(100);
-    delay(550);
-    motor1.stop();
-    motor2.stop();
+    right();
     if (ultraSonic.distanceCm() < 10) {
-      motor1.run(100);
-      motor2.run(100);
-      delay(1000);
-      motor1.stop();
-      motor2.stop();
+      fullTurn();
     }
+  }
+}
+void check() {
+  delay(1000);
+  right();
+  if (ultraSonic.distanceCm() < 10) {
+    fullTurn();
+    if (ultraSonic.distanceCm() < 10) {
+    right();
+    }
+  } else {
+    move();
   }
 }
 /*****************************/
@@ -44,5 +63,6 @@ void loop() {
   delay(300);
   move();
   turn();
+  check();
   }
 }
